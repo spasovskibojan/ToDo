022f3b1966fd60c955d38cbce7d262e6bd5a8539
spasovskibojan <122813316+spasovskibojan@users.noreply.github.com>
2023-06-05 20:55:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ function toDoShow(){
             proekti[i].classList.remove("aktiven");
         }
     }
+    let pros = document.querySelectorAll(".actives");
+    for(let i=0; i<pros.length; i++)
+    {
+        if(pros[i].classList.contains("actives"))
+        {
+            pros[i].classList.remove("actives");
+        }
+    }
 }
 
 
@@ -25,9 +33,20 @@ function showProject1(){
             proekti[i].classList.remove("aktiven");
         }
     }
+
+    let pros = document.querySelectorAll(".actives");
+    for(let i=0; i<pros.length; i++)
+    {
+        if(pros[i].classList.contains("actives"))
+        {
+            pros[i].classList.remove("actives");
+        }
+    }
     
     let project1 = document.getElementById("Project1");
     project1.classList.add("aktiven");
+    let pro1 = document.querySelector(".pro1");
+    pro1.classList.add("actives");
 }
 
 function showProject2(){
@@ -43,8 +62,19 @@ function showProject2(){
         }
     }
     
+    let pros = document.querySelectorAll(".actives");
+    for(let i=0; i<pros.length; i++)
+    {
+        if(pros[i].classList.contains("actives"))
+        {
+            pros[i].classList.remove("actives");
+        }
+    }
+
     let project1 = document.getElementById("Project2");
     project1.classList.add("aktiven");
+    let pro2 = document.querySelector(".pro2");
+    pro2.classList.add("actives");
 }
 
 
@@ -52,7 +82,7 @@ function showProject3(){
     let toDo = document.getElementById("ToDoMain");
     toDo.style.display="none";
 
-    let proekti = document.querySelectorAll(".project")
+    let proekti = document.querySelectorAll(".project");
     for(let i=0; i<proekti.length; i++)
     {
         if(proekti[i].classList.contains("aktiven"))
@@ -61,6 +91,19 @@ function showProject3(){
         }
     }
     
+
+
+    let pros = document.querySelectorAll(".actives");
+    for(let i=0; i<pros.length; i++)
+    {
+        if(pros[i].classList.contains("actives"))
+        {
+            pros[i].classList.remove("actives");
+        }
+    }
     let project3 = document.getElementById("Project3");
     project3.classList.add("aktiven");
+    let pro3 = document.querySelector(".pro3");
+    pro3.classList.add("actives");
 }
+
